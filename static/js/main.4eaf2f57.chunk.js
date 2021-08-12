a04f4594f64e8ff95058cb7481c731304d94159b
firecomputer <jay061116@gmail.com>
2021-08-12 14:08:25 +0900

    Updates

---
+++ static/js/main.4eaf2f57.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgajam_site_2021=this.webpackJsonpgajam_site_2021||[]).push([[0],{9:function(e,t,s){"use strict";s.r(t);var n=s(1),a=s.n(n),c=s(3),r=s.n(c),i=s(0);var j=function(){return Object(i.jsx)("div",{className:"App"})};r.a.render(Object(i.jsx)(a.a.StrictMode,{children:Object(i.jsx)(j,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.4eaf2f57.chunk.js.map
